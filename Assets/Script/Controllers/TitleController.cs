bbeb8badacc8a3370e5b74cf9ee5ddb03a3244c4
CanvasDEV-br <erickcanvas@gmail.com>
2021-07-25 08:49:08 -0300

    Add: Title scene placeholder version
    criacao de uma pasta GAME e TESTES na pasta scene, criacao da pasta CONTROLLERS na pasta de script, criacao do titulo placeholder

---
+++ Assets/Script/Controllers/TitleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+    public Button btnContinue;
+
+    private void Start()
+    {
+        //checar se existe um save e desabilitar ou habilitar a interacao    
+    }
+
+    public void Play()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
