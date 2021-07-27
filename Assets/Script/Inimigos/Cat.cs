97e9245280a1212ab98b0667757b79b29ee2a666
ErickTorres-CanvasDEV <erickcanvas@gmail.com>
2021-07-27 16:47:33 -0300

    Optimize Enemy Bull, Packages Imports, start cat enemy script
    Cinemachine and URP`imports
    Created script enemy bahaviour reusable

---
+++ Assets/Script/Inimigos/Cat.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public float timeDelayToTeleport = 2f;
+    public Transform target;
+    
+}
