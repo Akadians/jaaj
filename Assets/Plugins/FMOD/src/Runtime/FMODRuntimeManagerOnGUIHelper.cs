a7679ef493ebfaa87caa9bb9e195f685f7de8c7c
Isantanna <igor_sant@hotmail.com>
2021-07-27 22:10:23 -0300

    Fmod ocult
    FMODRuntimeManagerOnGUIHelper.cs
    
    TargetRunTimeManager.ExecuteOnGUI commented

--- Assets/Plugins/FMOD/src/Runtime/FMODRuntimeManagerOnGUIHelper.cs
+++ Assets/Plugins/FMOD/src/Runtime/FMODRuntimeManagerOnGUIHelper.cs
@@ -12,7 +12,7 @@ namespace FMODUnity
         {
             if (TargetRuntimeManager)
             {
-                TargetRuntimeManager.ExecuteOnGUI();
+                //TargetRuntimeManager.ExecuteOnGUI();
             }
         }
     }
