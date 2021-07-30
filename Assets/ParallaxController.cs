1b296e2c273e17dea338dcefe35e75aebb4606b3
Isantanna <igor_sant@hotmail.com>
2021-07-30 01:57:22 -0300

    Fase 01 - Disable
    Non finished ajudstments

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    public float parallaxEffect;
+    public Transform cam;
+
+    private float length;
+    private float startPos;
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;                 
+    }
+    void Update()
+    {
+        float Repos = cam.transform.position.x * (1 - parallaxEffect);
+        float Distance = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + Distance, transform.position.y, transform.position.z);
+
+        if (Repos > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (Repos < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
