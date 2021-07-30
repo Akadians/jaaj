fefa8c88ff620f0adae183a8492681f0e8acf70f
Isantanna <igor_sant@hotmail.com>
2021-07-30 11:00:01 -0300

    All Sounds and paths Title implemented

--- Assets/Script/Controllers/SoundController.cs
+++ Assets/Script/Controllers/SoundController.cs
@@ -1,32 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using FMODUnity;
+﻿using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    [FMODUnity.EventRef]
-    FMOD.Studio.EventInstance playerState;
-    //public string sfx_player_jump = "";
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PlayerJump()
     {
-        //playerState.start(StudioEventEmitter);
+        //FMODUnity.RuntimeManager.PlayOneShot("");
     }
     public void PlayerDeath()
     {
 
     }
+
+    public void ConfigButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sfx/ui/sfx_ui_menu_config");
+    }
+    public void CredButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sfx/ui/sfx_ui_menu_creditos");
+    }
+    public void ExitButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sfx/ui/sfx_ui_menu_exit");
+    }
+    public void ContinueButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sfx/ui/sfx_ui_menu_continue");
+    }
+    public void PlayButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sfx/ui/sfx_ui_menu_play");
+    }
+    public void BackButton()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/sfx/ui/sfx_ui_menu_volume_menos");
+    }
+
 }
