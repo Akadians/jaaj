6555d28b035a0f2e9bbc8bf41d5ed7f517f57b02
Isantanna <igor_sant@hotmail.com>
2021-07-31 19:22:52 -0300

    Fase 01 - Tuto

--- Assets/Script/TutorialController.cs
+++ Assets/Script/TutorialController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 public class TutorialController : MonoBehaviour
 {
     public GameObject PanelText;
+    public GameObject Player;
     public TextMeshProUGUI MainTex;
     public TextMeshProUGUI[] TutorialTex;
-    public int IdTex;    
+    public int IdTex;
 
     [SerializeField]
     private GameObject col01;
     [SerializeField]
     private GameObject col02;
+    [SerializeField]
+    private GameObject col03;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +27,7 @@ public class TutorialController : MonoBehaviour
         TutoControl();
     }
 
-    void TutoControl ()
+    void TutoControl()
     {
         MainTex.text = TutorialTex[IdTex].text;
 
@@ -33,41 +36,28 @@ public class TutorialController : MonoBehaviour
             IntroTuto();
             return;
         }
-        if(IdTex >= 3 && IdTex < 7)
+        if (IdTex >= 3 && IdTex < 6)
         {
             JumpTuto();
+        }
+        if (IdTex >= 6 && IdTex < 8)
+        {
+            CombatTuto();
+        }
+        if (IdTex >= 8 && IdTex < 11)
+        {
+            ActiveTuto();
         }
     }
     void IntroTuto()
     {
         if (PanelText.activeInHierarchy != false)
-        {          
-
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                MainTex.text = TutorialTex[IdTex].text;
-                if (IdTex == 2)
-                {
-                    PanelText.SetActive(false);
-                    IdTex++;
-                    return;
-                }                
-                IdTex++;
-                return;
-            }
-            return;            
-        }
-    }
-
-    void JumpTuto()
-    {
-        if (PanelText.activeInHierarchy != false)
         {
 
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 MainTex.text = TutorialTex[IdTex].text;
-                if (IdTex == 6)
+                if (IdTex == 2)
                 {
                     PanelText.SetActive(false);
                     IdTex++;
@@ -80,21 +70,90 @@ public class TutorialController : MonoBehaviour
         }
     }
 
+    void JumpTuto()
+    {
+        if (PanelText.activeInHierarchy != false)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                MainTex.text = TutorialTex[IdTex].text;
+                if (IdTex == 5)
+                {
+                    PanelText.SetActive(false);
+                    IdTex++;
+                    return;
+                }
+                IdTex++;
+                return;
+            }
+            return;
+        }
+    }
+
+    void CombatTuto()
+    {
+        if (PanelText.activeInHierarchy != false)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                MainTex.text = TutorialTex[IdTex].text;
+                if (IdTex == 7)
+                {
+                    PanelText.SetActive(false);
+                    IdTex++;
+                    Destroy(col02);
+                    return;
+                }
+                IdTex++;
+                return;
+            }
+            return;
+        }
+    }
+
+    void ActiveTuto()
+    {
+        if (PanelText.activeInHierarchy != false)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                MainTex.text = TutorialTex[IdTex].text;
+                if (IdTex == 10)
+                {
+                    PanelText.SetActive(false);
+                    Destroy(col03);
+                    return;
+                }
+                IdTex++;
+                return;
+            }
+            return;
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
         if (collision.gameObject.layer == 14 && IdTex < 4)
-        {          
-            PanelText.SetActive(true);            
+        {
+            PanelText.SetActive(true);
             Destroy(col01);
             return;
         }
 
-        if (collision.gameObject.layer == 14 && IdTex > 4)
+        if (collision.gameObject.layer == 14 && IdTex > 4 && IdTex < 8)
         {
-            PanelText.SetActive(true);
-            Destroy(col02);
+            PanelText.SetActive(true);            
+            return;
+        }
+
+        if (collision.gameObject.layer == 14 && IdTex > 7 && IdTex < 11)
+        {
+            PanelText.SetActive(true);            
             return;
         }
     }
